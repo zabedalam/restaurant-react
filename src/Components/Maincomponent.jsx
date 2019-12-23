d96095d57dcc0c46ef80ff47604ecf20d45e13a1
zabed <zabedalam777@gmail.com>
2019-12-23 16:25:46 +0100

    initialization

---
+++ src/Components/Maincomponent.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+class Maincomponent extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+               <p>Hello</p> 
+               
+               
+            </div>
+         );
+    }
+}
+ 
+export default Maincomponent;
